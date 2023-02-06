1f7ac3e2b6532b65bd6b7609d25594f2d841502f
arpitsha25 <arpitsharma1234x@gmail.com>
2023-02-06 22:43:56 +0530

    update

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -14,7 +14,7 @@ const projects = () => {
           <div className="pcontent">
             {/* --------------------------------------------------------- */}
             <Boxdata title="EVENTO" codeurl = "https://github.com/arpitsha25" demourl = "https://evento.onrender.com/"image={require('../images/ml.png')} />
-            <Boxdata title="PORTFOLIO" codeurl = "https://github.com/arpitsha25/portfolio-as" demourl = "https://arpitsha25.github.io/portfolio-as/" image={require('../images/ai.png')} />
+            <Boxdata title="PORTFOLIO" codeurl = "https://github.com/arpitsha25/portfolio-as" demourl = "https://arpitsharma.onrender.com/" image={require('../images/ai.png')} />
             <Boxdata title="WEATHER REPORT" codeurl = "https://github.com/arpitsha25/weather_app" demourl = "https://arpitsha25.github.io/weather_app/"image={require('../images/weather.jpg')} />
             {/* <Boxdata title="MOVIE LIST" codeurl = "https://github.com/arpitsha25" image={require('../images/tech.jpg')} /> */}
           </div>
