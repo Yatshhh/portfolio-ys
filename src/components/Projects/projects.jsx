30cbd0c375e244a8a1e819835e097dd5c256f389
Yatendra Sharma <111553919+Yatshhh@users.noreply.github.com>
2024-06-16 17:26:23 +0530

    Update projects.jsx

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import "./projects.css";
 import Title from "../title/Title";
 import Boxdata from "./Box-data/boxdata.jsx";
-// import { DiRequirejs } from "react-icons/di";
-// import Ai from "../images/ai.jpg";
+
 const projects = () => {
 
   return (
@@ -13,9 +12,9 @@ const projects = () => {
         <div className="main">
           <div className="pcontent">
             {/* --------------------------------------------------------- */}
-            <Boxdata title="EVENTO" codeurl = "https://github.com/arpitsha25" demourl = "https://evento.onrender.com/"image={require('../images/ml.png')} />
-            <Boxdata title="PORTFOLIO" codeurl = "https://github.com/arpitsha25/portfolio-as" demourl = "https://arpitsharma.onrender.com/" image={require('../images/ai.png')} />
-            <Boxdata title="WEATHER REPORT" codeurl = "https://github.com/arpitsha25/weather_app" demourl = "https://arpitsha25.github.io/weather_app/"image={require('../images/weather.jpg')} />
+            <Boxdata title="EVENTO" codeurl = "https://github.com/Yatshhh/Evento" demourl = "https://evento.onrender.com/"image={require('../images/ml.png')} />
+            <Boxdata title="PORTFOLIO" codeurl = "https://github.com/Yatshhh/portfolio-yss" demourl = "https://yatendrasharma.onrender.com/" image={require('../images/ai.png')} />
+            <Boxdata title="WEATHER REPORT" codeurl = "https://github.com/Yatshhh/weather_app" demourl = "https://github.com/Yatshhh.io/weather_app/"image={require('../images/weather.jpg')} />
             {/* <Boxdata title="MOVIE LIST" codeurl = "https://github.com/arpitsha25" image={require('../images/tech.jpg')} /> */}
           </div>
         </div>
