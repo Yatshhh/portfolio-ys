cc5428ff2914f21cc7845445f08604ed16f7447b
Yatendra Sharma <111553919+Yatshhh@users.noreply.github.com>
2024-06-22 19:23:50 +0530

    Update projects.jsx

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -14,7 +14,7 @@ const projects = () => {
             {/* --------------------------------------------------------- */}
             <Boxdata title="EVENTO" codeurl = "https://github.com/Yatshhh/Evento" demourl = "https://evento.onrender.com/"image={require('../images/ml.png')} />
             {/*  <Boxdata title="PORTFOLIO" codeurl = "https://github.com/Yatshhh/portfolio-yss" demourl = "https://yatendrasharma.onrender.com/" image={require('../images/ai.png')} /> */}
-            <Boxdata title="WEATHER REPORT" codeurl = "https://github.com/Yatshhh/weather_app" demourl = "https://github.com/Yatshhh.io/weather_app/"image={require('../images/weather.jpg')} />
+            <Boxdata title="WANDERLUST" codeurl = "https://github.com/Yatshhh/wanderlust" demourl = "https://github.com/Yatshhh/wanderlust/"image={require('../images/wanderlust.png')} />
             {/* <Boxdata title="MOVIE LIST" codeurl = "https://github.com/arpitsha25" image={require('../images/tech.jpg')} /> */}
           </div>
         </div>
