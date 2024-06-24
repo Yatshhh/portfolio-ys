44290cb86c04c7036010a9493e3105412a10d465
Yatendra Sharma <111553919+Yatshhh@users.noreply.github.com>
2024-06-24 08:48:44 +0530

    Update projects.jsx

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -14,7 +14,7 @@ const projects = () => {
             {/* --------------------------------------------------------- */}
             <Boxdata title="EVENTO" codeurl = "https://github.com/Yatshhh/Evento" demourl = "https://evento.onrender.com/"image={require('../images/ml.png')} />
             {/*  <Boxdata title="PORTFOLIO" codeurl = "https://github.com/Yatshhh/portfolio-yss" demourl = "https://yatendrasharma.onrender.com/" image={require('../images/ai.png')} /> */}
-            <Boxdata title="WANDERLUST" codeurl = "https://github.com/Yatshhh/wanderlust" demourl = "https://github.com/Yatshhh/wanderlust/"image={require('../images/wanderlust.png')} />
+            <Boxdata title="WANDERLUST" codeurl = "https://github.com/Yatshhh/wanderlust" demourl = "https://wanderlust-beta.vercel.app/"image={require('../images/wanderlust.png')} />
             {/* <Boxdata title="MOVIE LIST" codeurl = "https://github.com/arpitsha25" image={require('../images/tech.jpg')} /> */}
           </div>
         </div>
